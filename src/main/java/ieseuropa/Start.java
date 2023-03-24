93c3675c60b3baedcf4257cde197c6e05463ed2b
Usuario <Usuario@DESKTOP-0H2AA1P>
2023-03-24 13:34:01 +0100

    segundo

--- src/main/java/ieseuropa/Start.java
+++ src/main/java/ieseuropa/Start.java
@@ -1,10 +1,21 @@
 package main.java.ieseuropa;
+import java.util.Arrays;
 
 public class Start {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static double calcularMediana(int[] numeros) {
+        Arrays.sort(numeros);
+        int longitud = numeros.length;
+        if (longitud % 2 == 0) {
+            return (double) (numeros[longitud/2 - 1] + numeros[longitud/2]) / 2;
+        } else {
+            return numeros[longitud/2];
+        }
+    }
 
-	}
-
+    public static void main(String[] args) {
+        int[] numeros = {5, 7, 2, 10, 4, 1};
+        double mediana = calcularMediana(numeros);
+        System.out.println("La mediana de los números es: " + mediana);
+    }
 }
